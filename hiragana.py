5a9035eb905b8c16bc80abbba6bbe741baed202e
ShiatsuY <33645359+ShiatsuY@users.noreply.github.com>
2020-08-13 16:32:58 +0200

    kommentare hinzufügen

--- hiragana.py
+++ hiragana.py
@@ -1,8 +1,14 @@
+# test-wort zum übersetzen
 test = "aeiou"
 
+# for-schleife
+# jedes character des string (deswegen c in s) wird einzeln betrachtet
+# result ist der speicher, hier werden die konvertierten zeichen gespeichert
 def converter(s):
+	# speicher
 	result = ''
 
+	# for-schleife
 	for c in s:
 		if   c == 'a': result = result + 'あ'
 		elif c == 'e': result = result + 'え'
@@ -10,6 +16,8 @@ def converter(s):
 		elif c == 'o': result = result + 'お'
 		elif c == 'u': result = result + 'う'
 
+	# nach der for-schleife soll das ergebnis ausgegeben werden
 	print(result)
 
+# hier wird die funktion mit der eingabe des test-wortes aufgerufen
 converter(test)
