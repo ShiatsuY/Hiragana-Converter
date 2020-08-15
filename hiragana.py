6d2c7f91f44bf28d8040b2825babae3df14e3ad1
HinataHyuuga42 <69634510+HinataHyuuga42@users.noreply.github.com>
2020-08-15 13:58:13 +0200

    Update hiragana.py

--- hiragana.py
+++ hiragana.py
@@ -83,7 +83,6 @@ def converter(wort):
 				if wort[pos+2] == 'i':
 					result = result + 'し'
 					skip2 = True
-					
 
 			if wort[pos+1] == 'u':
 				result = result + 'す'
@@ -95,11 +94,214 @@ def converter(wort):
 				result = result + 'そ'
 				skip = True
 
+		# t-Reihe  ta chi! tsu! te to
+		if wort[pos] == 't':
+
+			# um den nächsten buchstaben schon zu betrachten benutzen wir einen kleinen trick:
+			# wir addieren eine 1 auf unseren zähler pos um den nächsten buchstaben zu erhalten
+			if wort[pos+1] == 'a': 
+				result = result + 'た'
+				# hier True setzen, weil wir schon den zweiten buchstaben betrachten!
+				skip = True
+			
+			# if wort[pos+1] == 'c':
+				result = result + 'ち'
+				skip = True
+				
+			if wort[pos+1] == 's':
+				skip = True
+				if wort[pos+2] == 'u':
+					result = result + 'つ'
+					skip2 = True
+
+			if wort[pos+1] == 'e':
+				result = result + 'て'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'と'
+				skip = True		
+
+
+		if wort[pos] == 'n':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'な'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'に'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'ぬ'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'ね'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'の'
+				skip = True
+
+
+		if wort[pos] == 'h':
+
+			# ha hi fu! he ho
+
+			if wort[pos+1] == 'a': 
+				result = result + 'は'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'ひ'
+				skip = True
+			#if wort[pos+1] == 'u':
+				result = result + 'ふ'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'へ'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'ほ'
+				skip = True
+
+
+		if wort[pos] == 'm':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'ま'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'み'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'む'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'め'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'も'
+				skip = True
+
+
+		if wort[pos] == 'y':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'や'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'ゆ'
+				skip = True		
+			if wort[pos+1] == 'o':
+				result = result + 'よ'
+				skip = True
+
+
+		if wort[pos] == 'r':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'ら'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'り'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'る'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'れ'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'ろ'
+				skip = True
+
+
+		if wort[pos] == 'w':
+			if wort[pos+1] == 'a':
+				result = result + 'わ'
+				skip = True
+
+			if wort[pos+1] == 'o':
+				result = result + 'を'
+				skip = True
+
+
+		if wort[pos] == 'g':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'が'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'ぎ'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'ぐ'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'げ'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'ご'
+				skip = True
+
+
+		if wort[pos] == 'b':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'ば'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'び'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'ぶ'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'べ'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'ぼ'
+				skip = True
+
+
+		if wort[pos] == 'p':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'ぱ'
+				skip = True
+			if wort[pos+1] == 'i':
+				result = result + 'ぴ'
+				skip = True
+			if wort[pos+1] == 'u':
+				result = result + 'ぷ'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'ぺ'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'ぽ'
+				skip = True
+
+
+		if wort[pos] == 'd':
+
+			if wort[pos+1] == 'a': 
+				result = result + 'だ'
+				skip = True
+			#if wort[pos+1] == 'i':
+				result = result + 'ぴ'
+				skip = True
+			#if wort[pos+1] == 'u':
+				result = result + 'ぷ'
+				skip = True
+			if wort[pos+1] == 'e':
+				result = result + 'で'
+				skip = True
+			if wort[pos+1] == 'o':
+				result = result + 'ど'
+				skip = True
 
 
 
 
-		# TODO: s-reihe hinzufügen
+		
 
 		if   wort[pos] == 'a': result = result + 'あ'
 		elif wort[pos] == 'i': result = result + 'い'
@@ -114,7 +316,7 @@ def converter(wort):
 
 # test-wort
 #test = "kakikukeko"
-test = "sashisuseso"
+test = "dadehipo"
 # hier wird die funktion mit der eingabe des test-wortes aufgerufen
 converter(test)
 
